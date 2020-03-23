b19cba2b339dfbc9b4d25f7447071332f272098d
Mirto111 <mitritdil@ya.ru>
2020-03-24 01:38:05 +0300

    5. Реализовать класс Tracker[#255709]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,29 +1,51 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
 
-    public Item add(Item item) {
-        item.setId(generateId());
-        items[size++] = item;
-        return item;
-    }
+  private final Item[] items = new Item[100];
+  private int ids = 1;
+  private int size = 0;
 
-    private String generateId() {
-        return String.valueOf(ids++);
-    }
+  public Item add(Item item) {
+    item.setId(generateId());
+    items[size++] = item;
+    return item;
+  }
 
-    public Item findById(String id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId().equals(id)) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+  private String generateId() {
+    return String.valueOf(ids++);
+  }
+
+  public Item findById(String id) {
+    Item rsl = null;
+    for (int index = 0; index < size; index++) {
+      Item item = items[index];
+      if (item.getId().equals(id)) {
+        rsl = item;
+        break;
+      }
     }
+    return rsl;
+  }
+
+  public Item[] findByName(String key) {
+    Item[] rsl = new Item[size];
+    int count = 0;
+    for (int index = 0; index < size; index++) {
+      Item item = items[index];
+      if (item.getName().equals(key)) {
+        rsl[count] = item;
+        count++;
+      }
+    }
+    rsl = Arrays.copyOf(rsl, count);
+    return rsl;
+  }
+
+  public Item[] findAll() {
+    Item[] itemsWithoutNull = Arrays.copyOf(items, size);
+    return itemsWithoutNull;
+  }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -13,4 +13,32 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
+    @Test
+    public void findByName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        Item item1 = new Item("test1");
+        Item another = new Item("test15");
+        tracker.add(item);
+        tracker.add(item1);
+        tracker.add(another);
+        Item[] result = tracker.findByName("test1");
+        Item[] expected = new Item[]{item, item1};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void findAll() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        Item item1 = new Item("test1");
+        Item another = new Item("test15");
+        tracker.add(item);
+        tracker.add(item1);
+        tracker.add(another);
+        Item[] result = tracker.findAll();
+        Item[] expected = new Item[]{item, item1, another};
+        assertThat(result, is(expected));
+    }
 }
