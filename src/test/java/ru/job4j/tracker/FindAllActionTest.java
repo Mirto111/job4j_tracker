9a2f79a27b62f1bfe374da466ad7fbc7eedac455
Mirto111 <mitritdil@ya.ru>
2020-03-26 04:31:08 +0300

    9.2. Тесты вывода на консоль в StartUI[#255643]

--- src/test/java/ru/job4j/tracker/FindAllActionTest.java
+++ src/test/java/ru/job4j/tracker/FindAllActionTest.java
@@ -8,33 +8,23 @@ import java.io.PrintStream;
 import java.util.StringJoiner;
 import org.junit.Test;
 
-public class StartUITest {
+public class FindAllActionTest {
 
   @Test
-  public void whenExit() {
-    StubInput input = new StubInput(
-        new String[]{"0"}
-    );
-    StubAction action = new StubAction();
-    new StartUI().init(input, new Tracker(), new UserAction[]{action});
-    assertThat(action.isCall(), is(true));
-  }
-
-  @Test
-  public void whenPrtMenu() {
+  public void whenCheckOutput() {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     PrintStream def = System.out;
     System.setOut(new PrintStream(out));
-    StubInput input = new StubInput(
-        new String[]{"0"}
-    );
-    StubAction action = new StubAction();
-    new StartUI().init(input, new Tracker(), new UserAction[]{action});
+    Tracker tracker = new Tracker();
+    Item item = new Item("fix bug");
+    tracker.add(item);
+    FindAllAction act = new FindAllAction();
+    act.execute(new StubInput(new String[]{}), tracker);
     String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
-        .add("Menu.")
-        .add("0. Stub action")
+        .add(item.toString() + " ")
         .toString();
     assertThat(new String(out.toByteArray()), is(expect));
     System.setOut(def);
   }
+
 }
--- src/test/java/ru/job4j/tracker/FindByNameActionTest.java
+++ src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -8,33 +8,25 @@ import java.io.PrintStream;
 import java.util.StringJoiner;
 import org.junit.Test;
 
-public class StartUITest {
+public class FindByNameActionTest {
 
   @Test
-  public void whenExit() {
-    StubInput input = new StubInput(
-        new String[]{"0"}
-    );
-    StubAction action = new StubAction();
-    new StartUI().init(input, new Tracker(), new UserAction[]{action});
-    assertThat(action.isCall(), is(true));
-  }
-
-  @Test
-  public void whenPrtMenu() {
+  public void whenCheckOutput() {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     PrintStream def = System.out;
     System.setOut(new PrintStream(out));
-    StubInput input = new StubInput(
-        new String[]{"0"}
-    );
-    StubAction action = new StubAction();
-    new StartUI().init(input, new Tracker(), new UserAction[]{action});
+    Tracker tracker = new Tracker();
+    Item item = new Item("fix bug");
+    Item item1 = new Item("fix another bug");
+    tracker.add(item);
+    tracker.add(item1);
+    FindByNameAction act = new FindByNameAction();
+    act.execute(new StubInput(new String[]{"fix bug"}), tracker);
     String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
-        .add("Menu.")
-        .add("0. Stub action")
+        .add(item.toString() + " ")
         .toString();
     assertThat(new String(out.toByteArray()), is(expect));
     System.setOut(def);
   }
+
 }
